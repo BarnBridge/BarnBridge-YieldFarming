f4bd06cc0b1d9d0feb08aefb835bcf8b0c4c57d1
Casian Lacatusu <casian.lacatusu@gmail.com>
2020-09-04 18:05:06 +0300

    Fix import path for IERC20.

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Vault is Ownable {
     /*
