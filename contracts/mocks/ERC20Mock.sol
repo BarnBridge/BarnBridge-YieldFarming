c6e6a067a3d67f0cc84134032c00dc93ca8f2a99
Casian Lacatusu <casian.lacatusu@gmail.com>
2020-09-05 14:05:16 +0300

    Add one more test. Refactor. Add a ERC20Mock.

---
+++ contracts/mocks/ERC20Mock.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ERC20Mock is ERC20("ERC20Mock", "MCK") {
+}
--- test/Vault.test.js
+++ test/Vault.test.js
@@ -5,19 +5,24 @@ use(require('chai-bignumber')())
 
 describe('Vault', function () {
     let vault
+    let erc20Mock
     let owner, user
 
     const DAIAddr = '0x6b175474e89094c44da98b954eedeac495271d0f'
 
     beforeEach(async function () {
-        const [signer, anotherSigner, userSigner] = await ethers.getSigners()
-        owner = anotherSigner._address
-        user = userSigner._address
+        const [creator, ownerSigner, userSigner] = await ethers.getSigners()
+        owner = ownerSigner
+        user = userSigner
 
-        const Vault = await ethers.getContractFactory('Vault', signer)
+        const Vault = await ethers.getContractFactory('Vault', creator)
 
-        vault = await Vault.deploy(owner)
+        vault = await Vault.deploy(await ownerSigner.getAddress())
         await vault.deployed()
+
+        const ERC20Mock = await ethers.getContractFactory('ERC20Mock')
+        erc20Mock = await ERC20Mock.deploy()
+        await erc20Mock.deployed()
     })
 
     it('Can deploy successfully', async function () {
@@ -25,12 +30,27 @@ describe('Vault', function () {
     })
 
     it('Transfers ownership to address provided', async function () {
-        expect(await vault.owner()).to.equal(owner)
+        let ownerAddr = await owner.getAddress()
+        expect(await vault.owner()).to.equal(ownerAddr)
     })
 
     describe('stake', function () {
-        it('can only be executed by owner', async function () {
-            await expect(vault.stake(DAIAddr, 100, user)).to.be.reverted
+        it('reverts if not executed by owner', async function () {
+            let userAddr = await user.getAddress()
+            await expect(vault.stake(DAIAddr, 100, userAddr)).to.be.reverted
         })
+
+        it('succeeds if called by owner', async function() {
+            let userAddr = await user.getAddress()
+
+            await expect(vault.connect(owner).stake(DAIAddr, 100, userAddr)).to.not.be.reverted
+        })
+
+        // todo:
+        // 1. save the staked value for the user
+        // -- it should allow and keep track of multiple erc20 tokens
+        // 2. call transferFrom on the erc20 token to transfer the amount to itself
+        // -- should it check for allowance first or use a try-catch block?
+        // 3. what should the function return?
     })
 })
