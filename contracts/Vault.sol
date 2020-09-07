571b5a1d7c994f854cbee925fee3e557c3a589bd
Casian Lacatusu <casian.lacatusu@gmail.com>
2020-09-07 11:44:51 +0300

    Add comments for withdraw and stakeBalanceOf functions.

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Vault is Ownable {
-    mapping (address => mapping (address => uint256)) private stakes;
+    mapping(address => mapping(address => uint256)) private stakes;
 
     /*
      * Sets the owner to the `_owner` address supplied. This should be the main YieldFarming contract address.
@@ -28,10 +28,16 @@ contract Vault is Ownable {
         token.transferFrom(user, address(this), amount);
     }
 
+    /*
+     * Returns the amount of `token` that the `user` has currently staked
+     */
     function stakeBalanceOf(address user, address token) public view returns (uint256) {
         return stakes[user][token];
     }
 
+    /*
+     * Removes the stake of the user and sends the staked amount of `token` back to the `user`
+     */
     function withdraw(IERC20 token, address user) public onlyOwner {
         require(stakes[user][address(token)] > 0, "Vault: User has no stake");
 
