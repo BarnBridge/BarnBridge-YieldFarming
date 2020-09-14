47bc4e6dd17e79becf3f49e909c5416483b4d699
Stefan <SAB>
2020-09-14 08:48:35 +0300

    Fixed tests with dependency contract.

--- test/YieldFarm.test.js
+++ test/YieldFarm.test.js
@@ -1,11 +1,30 @@
 const { expect } = require('chai');
 const { ethers } = require('@nomiclabs/buidler');
 
+
 describe('YieldFarm', function () {
     let yieldFarm
+    let vault
+    let owner, user
+    const barnBridge = '0x07865c6E87B9F70255377e024ace6630C1Eaa37F'
+    const usdc = '0x07865c6E87B9F70255377e024ace6630C1Eaa37F'
+    const susd = '0x07865c6E87B9F70255377e024ace6630C1Eaa37F'
+    const dai = '0x07865c6E87B9F70255377e024ace6630C1Eaa37F'
+    const barnYCurve = '0x07865c6E87B9F70255377e024ace6630C1Eaa37F'
     beforeEach (async function () {
+        const [creator, ownerSigner, userSigner] = await ethers.getSigners()
+        owner = ownerSigner
+        ownerAddr = await owner.getAddress()
+
+        user = userSigner
+        userAddr = await user.getAddress()
+
+        const Vault = await ethers.getContractFactory('Vault', creator)
+
+        vault = await Vault.deploy(await ownerSigner.getAddress())
+        await vault.deployed()
         const YieldFarm = await ethers.getContractFactory('YieldFarm')
-        yieldFarm = await YieldFarm.deploy()
+        yieldFarm = await YieldFarm.deploy(barnBridge, usdc, susd, dai, barnYCurve, vault.address)
         await yieldFarm.deployed()
     })
     it("should compute bonus", async function () {
